0bbc9200499abee16c8099aa445c169d02053858
ShadowNinja <shadowninja@minetest.net>
2017-04-22 12:21:38 -0400

    Fix Windows support

--- src/util.hpp
+++ src/util.hpp
@@ -8,12 +8,11 @@
 #define ARRAY_SIZE(a) (sizeof(a)/sizeof(*a))
 
 #ifdef _WIN32
+#	define WIN32_LEAN_AND_MEAN
+#	include <windows.h>
+#	include <io.h>
 #	define fileno(x) _fileno(x)
-inline bool isatty(int fd)
-{
-	DWORD mode;
-	return GetConsoleMode((HANDLE)_get_osfhandle(fd), &mode);
-}
+#	define isatty(x) _isatty(x)
 #endif
 
 
